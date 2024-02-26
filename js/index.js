cd1da19a9dd341bf13c668dcdec4f3cf5f7b93f6
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-02-26 17:50:56 +0600

    score & highScore added

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('../music/food.mp3');
 const gameOverSound = new Audio('../music/gameover.mp3');
 const moveSound = new Audio('../music/move.mp3')
 const musicSound = new Audio('../music/music.mp3')
-let speed = 5;
+let speed = 8;
 let score = 0;
 let lastPaintTime= 0;
 let snakeArr =[
@@ -58,6 +58,11 @@ function gameEngine(){
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play()
         score +=1;
+        if(score > hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
+        }
         scoreBox.innerHTML = ' SCORE:' + score +'!!!';
         snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y + inputDir.y });
         let a = 3;
@@ -109,7 +114,16 @@ function gameEngine(){
 
 
 
-// main logics start naw 😎😎
+// main logics start now 😎😎
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore === null){
+    hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+}
+else{
+    hiscoreval = JSON.parse(hiscore);
+    hiscoreBox.innerHTML = "HiScore: " + hiscore;
+}
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e=>{
     inputDir = {x:0, y:1} // start the game
